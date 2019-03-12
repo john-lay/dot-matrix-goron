03090b6e9a2b16514da9f616d0d8bc02e98a8e93
john-lay <john_lay@hotmail.co.uk>
2019-03-12 21:10:28 +0000

    Add smiley face tile
    Modified a medium tutorial to make it
    compatible with rgbasm.
    
    `https://medium.com/@ulrikdamm/writing-your-first-gameboy-game-4ea62c76db29`

--- source/main.asm
+++ source/main.asm
@@ -11,4 +11,34 @@ REPT $150 - $104
 ENDR
 
 SECTION "Game code", ROM0
+
 Start:
+ ; Copy the sprite data into VRAM
+ ld hl, $9000 + 16
+ ld de, smiley_sprite
+ ld b, 16
+ 
+copy_loop:
+  ld a, [de]
+  inc de
+  ld [hl+], a
+  dec b
+  jp nz, copy_loop
+   
+ ; Set the first byte of the background tile map
+ ld hl, $9800
+ ld [hl], 1
+ 
+ ; Set the background palette
+ ld hl, $ff47
+ ld [hl], %11100100
+ 
+ ; Set LCDC to turn on the display
+ ld hl, $ff40
+ ld [hl], %10000001
+ 
+ ; Stop execution
+.end 
+    jp .end
+
+smiley_sprite: db $00, $00, $00, $00, $24, $24, $00, $00, $81, $81, $7e, $7e, $00, $00, $00, $00
