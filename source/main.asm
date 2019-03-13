8d8768aed0ab99d75f5530e8bf47516cbb5e2e9c
john-lay <john_lay@hotmail.co.uk>
2019-03-13 20:31:05 +0000

    Add a rupee sprite

--- source/main.asm
+++ source/main.asm
@@ -15,7 +15,7 @@ SECTION "Game code", ROM0
 Start:
  ; Copy the sprite data into VRAM
  ld hl, $9000 + 16
- ld de, smiley_sprite
+ ld de, rupee
  ld b, 16
  
 copy_loop:
@@ -42,3 +42,4 @@ copy_loop:
     jp .end
 
 smiley_sprite: db $00, $00, $00, $00, $24, $24, $00, $00, $81, $81, $7e, $7e, $00, $00, $00, $00
+rupee: db $0f, $0f, $11, $11, $21, $21, $47, $49, $4e, $72, $4c, $74, $78, $78
